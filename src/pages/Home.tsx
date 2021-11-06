3c624c89e28bc16421d662a9006340e34ad18f24
vietvd13 <vuducviet0131@gmail.com>
2021-11-06 17:48:23 +0700

    Add ring bell

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,9 +8,7 @@ import {
   IonItem,
   IonButton 
 } from '@ionic/react';
-
 import ReactAudioPlayer from 'react-audio-player';
-
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -35,8 +33,12 @@ const Home: React.FC = () => {
           </IonText>
         </IonItem>
         <IonItem>
-          <IonButton onClick={() => musicPlayer?.audioEl.current?.play()}>Play music</IonButton>
-          <IonButton onClick={() => musicPlayer?.audioEl.current?.pause()}>Pause music</IonButton>
+          <IonButton 
+            onClick={() => musicPlayer?.audioEl.current?.play()}
+          >Play music</IonButton>
+          <IonButton 
+            onClick={() => musicPlayer?.audioEl.current?.pause()}
+          >Pause music</IonButton>
 
           <ReactAudioPlayer
             ref={(element) => { musicPlayer = element; }}
@@ -51,7 +53,28 @@ const Home: React.FC = () => {
           </IonText>
         </IonItem>
         <IonItem>
-          <IonButton onClick={() => navigator.vibrate(2000)}>Vibrate</IonButton>
+          <IonButton 
+            onClick={() => navigator.vibrate(2000)}
+          >Vibrate</IonButton>
+        </IonItem>
+
+        <IonItem>
+          <IonText>
+            <h4>Ring a Bell</h4>
+          </IonText>
+        </IonItem>
+        <IonItem>
+          <IonButton
+            onClick={() => {
+              musicPlayer?.audioEl.current?.play();
+              
+              navigator.vibrate(5000);
+
+              setTimeout(() => {
+                musicPlayer?.audioEl.current?.pause()
+              }, 5000);
+            }}
+          >Ring Bell</IonButton>
         </IonItem>
       </IonContent>
     </IonPage>
